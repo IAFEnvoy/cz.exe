2e100512f876299f952981a29533839ef20309c1
= <=>
2022-02-24 12:48:56 +0800

    1

---
+++ app/src/main/java/net/iafenvoy/cz/People/teachers/王华.java
@@ -0,0 +1,31 @@
+package net.iafenvoy.cz.People.teachers;
+
+import net.iafenvoy.cz.People.老师;
+import net.iafenvoy.cz.Utils.Enum.Sex;
+
+public class 王华 extends 老师 {
+
+    public 王华() {
+        super("王华", "社会你华姐", -1,Sex.Female, -1, -1, "英语组组长","高一1,3班英语老师");
+    }
+
+    public void 来一句语录(){
+
+    }
+
+    public void 默写(){
+
+    }
+
+    public void DailyTalk(){
+
+    }
+
+    public void 切换语言(){
+
+    }
+
+    public void 发火(){
+        
+    }
+}
